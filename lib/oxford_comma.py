dcb5f29f8dd56a07a6e6df0a3b7cdc74526a1005
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-02-06 16:42:39 -0500

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return ''.join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    elif len(items) == 3:
+       return f"{items[0]}, {items[1]}, and {items[2]}"
+    elif len(items) > 3:
+        item_names = items[0:-1]
+        return ", ".join(item_names) + f", and {items[-1]}"
